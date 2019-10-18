0ecc47c71df99c47ca0c1bd158c213615851b9cc
Your Name <you@example.com>
2019-10-17 21:06:11 -0400

    Iniciando projeto

---
+++ FindBookCrossCutting/BookInjector.cs
@@ -0,0 +1,23 @@
+﻿using FindBookAplication;
+using FindBookDomain.Aplication;
+using FindBookDomain.Repository;
+using FindBookRepository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+// esta classe esta chamando la no Startup.cs
+namespace FindBookCrossCutting
+{
+    public class BookInjector
+    {
+        public static void RegistrarServicos(IServiceCollection services)
+        {
+            //Repositories
+            services.AddScoped<IBookRepository, BookRepository>();
+
+            //Services
+            services.AddScoped<IBookService, BookService>();
+        }
+
+    }
+}
